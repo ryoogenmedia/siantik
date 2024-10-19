684073c89484b4fd7e32013975fb0dd24926a3ce
Bintang. <90763575+M-BintangR@users.noreply.github.com>
2024-10-19 13:20:30 +0800

    footer fix for save data ✅

--- resources/views/components/backend/button/save.blade.php
+++ resources/views/components/backend/button/save.blade.php
@@ -1,7 +1,9 @@
-<button type="submit" class="btn tf-btn primary my-3 {{ $class ?? '' }}" wire:loading.attr="disabled">
-    <span wire:loading.remove wire:target="{{ $target }}">{{ $name ?? 'Simpan' }}</span>
+<div class="footer-fixed p-16">
+    <button type="submit" class="btn tf-btn primary my-3 {{ $class ?? '' }}" wire:loading.attr="disabled">
+        <span wire:loading.remove wire:target="{{ $target }}">{{ $name ?? 'Simpan' }}</span>
 
-    <span wire:loading wire:target="{{ $target }}">Memuat</span>
+        <span wire:loading wire:target="{{ $target }}">Memuat</span>
 
-    <span class="animated-dots" wire:loading wire:target="{{ $target }}"></span>
-</button>
+        <span class="animated-dots" wire:loading wire:target="{{ $target }}"></span>
+    </button>
+</div>
