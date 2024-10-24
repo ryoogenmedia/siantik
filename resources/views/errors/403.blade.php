25a0dbb0eb3cf587b840816509362afa79567a0b
Fery Fadul Rahman <68836194+RECTY45@users.noreply.github.com>
2024-10-24 15:02:14 +0800

    little change

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -29,7 +29,7 @@
             </a>
         </div>
 
-        <form id="logout-form" action="{{ route('mobile.logout') }}" method="POST" style="display: none;">
+        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
             @csrf
         </form>
     </div>
